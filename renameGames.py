140418a131bebd13388984f9656c54c011ccf7c5
Kyle Swygert <kyle.swygert@wsu.edu>
2019-11-09 20:33:19 -0800

    working on creating a GUI for picking and renaming files. for better user experience.

--- renameGames.py
+++ renameGames.py
@@ -14,9 +14,8 @@
 NOTE: there may be a difference when parsing replays captured from a Wii vs a connection to a PC. Make sure that the PC captured replays are parsed the same as the Wii captured files. 
 
 TODO: Rename replays against CPUs using the tag CPU for the character to make sure it is not a human player. 
-TODO: create functions generate_singles_game_name() and generate_doubles_game_name()
 
-
+TODO: generate file that lists the files that caused and error and the error that was caused. 
 
 '''
 
@@ -90,7 +89,7 @@ def generate_doubles_game_name(slippiFile):
 
     # players have been separated by team, now build the list of strings for the team names. 
 
-    firstPlayer = False
+    #firstPlayer = False
 
 
     # build the first team list of strings
@@ -233,8 +232,6 @@ def generate_singles_game_name(slippiFile):
     '''
     this function will build a string based on the characters used in the game. 
 
-    
-
     '''
 
     newFile = ''
@@ -285,6 +282,13 @@ def rename_files_in_folder(folder):
     '''
 
     print('folder that contains all the slippi files: {}'.format(folder))
+
+    # check that the folder exists in the system. 
+    if path.exists(folder) == False:
+        print(f'{folder} does not exist in the system')
+        return
+
+    print(f'{folder} exists, renaming all files in the folder...')
 
     for root, dirs, files in walk(folder):
         # root represents the current directory that is being processed
@@ -402,10 +406,10 @@ def rename_files_in_folder(folder):
                                     #print('successfully renamed the file')
 
                                 except:
-                                    print('cound not rename the file')
+                                    print(f"Can't RENAME: {currFilePath}")
 
                             except:
-                                print("could not process file {}".format(curr))
+                                print(f"Can't process SINGLES: {currFilePath}")
                             
                     else:
                     
@@ -419,122 +423,33 @@ def rename_files_in_folder(folder):
                             rename(currFilePath, doubles)
 
                         except:
-                            print('could not process this doubles game: {}'.format(curr))
+                            print(f"Can't process TEAMS: {currFilePath}")
 
                 except:
                     # TODO: look into why the renaming program breaks here. what exception is being thrown? This might be because of the replay being cut off when the connection was cut. see if there is any way to tell if a file is malformed...
-                    print('{} broke the execution for some reason, look into why that happened...'.format(currFilePath))
 
+                    # TODO: look into if this block is reached ONLY if the file is corrupt. 
+
+                    print(f'CORRUPTED: {currFilePath}')
                     
 
-                    # NOTE: i believe that when I file reaches here with an exception, that file is then deleted from the directory... I dont want that to happen. Make sure that the file is preserved even when an exception has occured. 
-
-                    pass # TODO: is this pass statement required here? 
-
-
             else:
-                print('{} was wrong file format'.format(curr))
+                print(f'WRONG FORMAT: {currFilePath}')
+
+
+    print(f"finished renaming files in {folder}")
 
 
 # NOTE: this is where the program starts executing when run as a command line program. 
 # TODO: change the folder input parameter for the function to be based on the command line argument that the user has given rather than being a hard coded directory name. 
 #rename_files_in_folder('.\slp') # NOTE: '/' for linux, '\' for windows. 
 
-directory = path.join(path.dirname(path.realpath(__file__)), 'slp')
-
-rename_files_in_folder(directory)
-
-"""
-# iterate over the files in a directory
-for filename in listdir('./slp'):
-
-    try:
-
-        # open the game file, get the characters, and get the tags if they exist.
-
-        dst = ''
-        src = ''
-
-        tempGame = Game("slp/" + filename)
+# NOTE: these function calls below will only execute when the file itself is executed. When the file is exported as a module, then these function calls will not execute automatically. 
+if __name__ == "__main__":
 
 
-        if tempGame.start.is_teams == True:
-            # TODO: Implement renaming of Teams games
-            print("{} is a Teams game, cannot rename.".format(filename))
-            # Team Naming Guide Idea: TeamGreen(Falco+(H E L P))-Vs-TeamBlue(CaptainFalcon+(S E L F))<datetime>.slp
+    directory = path.join(path.dirname(path.realpath(__file__)), 'slp')
 
-        else:
+    rename_files_in_folder(directory)
 
-            # Count number of players in current game
-            currentPlayers = 0
-            for player in tempGame.start.players:
-                if player != None:
-                    currentPlayers += 1
-
-            if currentPlayers != 2:
-                # TODO: implement renaming free-for-all matches with just character names, not tags.
-                print("{} is an FFA or Teams with more than 2 players, cannot rename.".format(
-                    filename))
-
-            else:
-                # there are only 2 players in the game, rename the file. 
-
-                #print(generate_file_name(tempGame) + '\n\n')
-
-                '''
-                # list used to build the new Filename
-                newFileNameParts = []
-
-                firstChar = False
-
-                #curPlatform = tempGame.metadata.platform
-
-                # iterate over players in the game
-                for player in tempGame.start.players:
-
-                    # append the formatted character name from the character object to the list
-                    [newFileNameParts.append(item.lower().capitalize()) for item in str(
-                        player.character).split('.')[-1].split('_')] if player != None else None
-
-                    # append the player tag to the list if they were using one
-                    newFileNameParts.append(
-                        '(' + player.tag + ')') if player != None and player.tag != '' else None
-
-                    # append '-Vs-' string to list if this is the first player being added to the list
-                    if newFileNameParts != [] and firstChar == False:
-                        firstChar = True
-                        newFileNameParts.append('-Vs-')
-
-
-                # Build the new Filename
-                for item in newFileNameParts:
-                    dst += item
-
-                # appending the date and time that the game was played to reduce chance of overwriting files
-                # TODO: remove the trailing time part that is added when processing files captured from a PC.
-                    # reorganize the program: have a flag for processing Console Replays and processing PC replays. 
-
-                
-                # Could I do list comprehension here to replace the multiple different characters with nothing?
-                dst += '<' + str(tempGame.metadata.date).split('+')[0].replace(
-                    ' ', '').replace('-', '').replace(':', '').split('.')[0] + '>'
-
-                '''
-
-                dst = generate_file_name(tempGame)
-
-                dst += '.slp'
-
-                src = 'slp/' + filename
-                dst = 'slp/' + dst
-
-                rename(src, dst)
-
-                newFileNameParts.clear()
-
-    except:
-        print("{} cannot be processed, skipping file.".format(filename))
-
-"""
-
-print("All files in the directory have been renamed.")
+    print("All files in the directory have been renamed.")
